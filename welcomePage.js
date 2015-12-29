7e7b17f67b6536134659a1671d53125d32f198d8
iyud <yudkovich.i@husky.neu.edu>
2015-12-29 18:04:05 -0500

    changed front page, changed other pages banners to be 100% instead of 1920px

--- welcomePage.js
+++ welcomePage.js
@@ -1,19 +1,19 @@
 var main = function() {
     $('#icon-menu').click(function(){
         $('#menu').animate({
-            right: '0px'
+            left: '0px'
         }, 200);
         $('body').animate({
-        right: '285px'
+        left: '285px'
     }, 200);
     });
     
     $('#icon-close').click(function() {
         $('#menu').animate({
-            right: '-285px'
+            left: '-285px'
         }, 200);
         $('body').animate({
-            right: '0px'
+            left: '0px'
         }, 200);
     });
     
